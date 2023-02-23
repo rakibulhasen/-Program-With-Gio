e2d2d3a632c68cc4ff94fee67da642e7146b1b87
Rakibul <rakibul.4658@gmail.com>
2023-02-23 14:41:20 +0600

    Operator  One

---
+++ Basic-PHP/operators.php
@@ -0,0 +1,95 @@
+<?php
+
+/* OPERATORS  */
+
+// Arithmetic Operators
+// $x = 10;
+// $y = 2;
+// var_dump($x + $y);
+// var_dump($x - $y);
+// var_dump($x / $y);
+// var_dump($x * $y);
+// var_dump($x ** $y);
+
+// // With Negative Number
+// $x2 = -10;
+// $y2 = 2;
+
+// var_dump($x2 * $y2);
+// var_dump($x2 - $y2);
+
+// $x2 = 10;
+// $y2 = -2;
+
+// var_dump($x2 * $y2);
+// var_dump($x2 - $y2);
+
+// // Plus and Minus Operators
+// $x = +'10';
+// $y = 2;
+// var_dump($x, $y);
+
+// $x = -'10';
+// $y = 2;
+// var_dump($x, $y);
+
+
+// Second tip is about division
+
+// $x = 10;
+// $y = 2;
+// var_dump($x / $y);
+
+// $x = 10;
+// $y = 3;
+// var_dump($x / $y);
+
+// $x = 10;
+// $y = 2.0;
+// var_dump($x / $y); 
+
+
+
+// $x = 10;
+// $y = 0;
+// var_dump($x / $y);  // You will see and error zero not devided 
+
+
+// // Reduce error
+// $x = 10;
+// $y = 0;
+// var_dump(fdiv($x, $y));  // You will see and error zero not devided  
+
+// Modulus Oparators
+// $x = 10;
+// $y = 2;
+// var_dump($x % $y);  
+
+// Casting as integer
+// $x = 10;
+// $y = 6;
+// var_dump($x % $y);  
+
+// $x = 10.5;
+// $y = 2.9;
+// var_dump($x % $y);   
+
+// fmod function
+// $x = 10.5;
+// $y = 2.9;
+// var_dump(fmod($x, $y));   
+
+// Negative 
+// $x = 10;
+// $y = -3;
+// var_dump($x % $y);  
+
+$x = -10;
+$y = 3;
+var_dump($x % $y);    
+
+
+
+
+
+
